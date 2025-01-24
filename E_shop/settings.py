7390253234e0ea5e08e75c21ad36c9615005410e
sub <subekshya.211239@ncit.edu.np>
2025-01-24 12:15:07 +0545

    Issue Fixed

--- E_shop/settings.py
+++ E_shop/settings.py
@@ -69,7 +69,7 @@ WSGI_APPLICATION = 'E_shop.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR/'db.sqlite3',
     }
 }
 
